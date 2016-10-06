9548deba621726f0a92e34477414d9ec218ad866
Stanislav Vlasic <svlasic@gmail.com>
2016-10-06 03:07:29 +0200

    Update

--- main.py
+++ main.py
@@ -1,23 +1,10 @@
 import sys
-import xbmc
-import xbmcaddon
-
-import os
-import os.path
-
-ID    = 'script.module.extrocore'
-ADDON = xbmcaddon.Addon(ID)
-PATH  = ADDON.getAddonInfo("path")
-LIB   = xbmc.translatePath( os.path.join(PATH, 'lib' ) ).decode("utf-8")
-
-sys.path.append(LIB)
-
 import ExtroCore
 
 if len(sys.argv) > 1 :
   if sys.argv[1] == 'rebootAndroid':
-      rebootAndroid()
+      ExtroCore.rebootAndroid()
   elif sys.argv[1] == 'rebootLinux':
-      rebootLinux()
+      ExtroCore.rebootLinux()
   elif sys.argv[1] == 'rebootDebian':
-      rebootDebian()
+      ExtroCore.rebootDebian()
